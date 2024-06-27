d45ea9ef8fce3bfb3c0364f269ef06aac109f5ae
luigimicco <luigi.micco@gmail.com>
2024-06-27 20:57:42 +0200

    feat: add borders

--- lib/logic/cell.dart
+++ lib/logic/cell.dart
@@ -1,9 +1,20 @@
 import 'coords.dart';
 
+// posizione dei bordi
+enum Side { t, r, b, l } // top, bottom, left, right
+
 class Cell {
   Cell({
     required this.coords,
   });
 
   final Coords coords;
+
+  // ogni cella nasce con i bordi su tutti i lati
+  final List<Side> borders = <Side>[
+    Side.t,
+    Side.r,
+    Side.b,
+    Side.l,
+  ];
 }
