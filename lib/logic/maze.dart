add9cbac644ef22bf76bbf130463330d2168c9f8
luigimicco <luigi.micco@gmail.com>
2024-06-27 20:31:33 +0200

    feat: start grid

--- lib/logic/maze.dart
+++ lib/logic/maze.dart
@@ -1,4 +1,5 @@
 import 'cell.dart';
+import 'coords.dart';
 
 class Maze {
   // dimensione della griglia (ipotesi che sia quadrata)
@@ -10,11 +11,26 @@ class Maze {
   Maze({
     required this.size,
     required this.cellWidth,
-  }) {}
+  }) {
+    // griglia iniziale
+    _createGrid();
+  }
 
   // array delle celle
   final List<List<Cell>> grid = <List<Cell>>[];
 
   int get rows => (size / cellWidth).floor();
   int get columns => (size / cellWidth).floor();
+
+  void _createGrid() {
+    grid.clear();
+
+    for (var i = 0; i < columns; i++) {
+      grid.add(<Cell>[]);
+
+      for (var j = 0; j < rows; j++) {
+        grid[i].add(Cell(coords: Coords(i, j)));
+      }
+    }
+  }
 }
